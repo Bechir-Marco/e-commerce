7822ea4a8654a5005b0f74826efedab376c6ca1d
Bechir-Marco <bechir.marko@gmail.com>
2023-08-15 00:06:03 +0100

    Home page Stlye

--- src/components/FooterComponent.tsx
+++ src/components/FooterComponent.tsx
@@ -1,6 +1,18 @@
+import { Container, Row, Col } from "react-bootstrap";
+
 const FooterComponent = () => {
     return (
-        <div> This is FooterComponent Page </div>
+        <footer>
+            <Container fluid>
+                <Row className="mt-5">
+                    <Col className="bg-dark text-white text-center py-5">
+                        Copyright &copy; Best Online Shop
+                    </Col>
+                </Row>
+            </Container>
+        </footer>
     );
 };
+
 export default FooterComponent;
+
