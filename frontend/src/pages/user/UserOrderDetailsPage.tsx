a6eae9868395cf4b53eaab8368dba1d2de389fb1
Bechir-Marco <bechir.marko@gmail.com>
2023-10-08 14:37:10 +0100

    user order with paypal integration

--- frontend/src/pages/user/UserOrderDetailsPage.tsx
+++ frontend/src/pages/user/UserOrderDetailsPage.tsx
@@ -1,9 +1,113 @@
-import UserOrderDetailsPageComponent from "./components/UserOrderDetailsPageComponent";
+import UserOrderDetailsPageComponent from './components/UserOrderDetailsPageComponent';
+import { useSelector } from 'react-redux';
+import axios from 'axios';
+import { loadScript } from '@paypal/paypal-js';
+
+
+const getOrder = async (orderId:number) => {
+  const { data } = await axios.get('/api/orders/user/' + orderId);
+  return data;
+};
+
+const loadPayPalScript = (
+  cartSubtotal: any,
+  cartItems: any[],
+  orderId: any,
+  updateStateAfterOrder: (arg:any)=>void
+) : void => {
+  loadScript({ 'client-id': 'your PayPal client id' })
+    .then((paypal :any) => {
+      paypal
+        .Buttons(
+          buttons(cartSubtotal, cartItems, orderId, updateStateAfterOrder)
+        )
+        .render('#paypal-container-element');
+    })
+    .catch((err) => {
+      console.error('failed to load the PayPal JS SDK script', err);
+    });
+};
+
+const buttons = (cartSubtotal: number, cartItems: any[], orderId: number, updateStateAfterOrder: (arg0: any) => void) => {
+  return {
+    createOrder: function (data:any, actions:any) {
+      return actions.order.create({
+        purchase_units: [
+          {
+            amount: {
+              value: cartSubtotal,
+              breakdown: {
+                item_total: {
+                  currency_code: 'USD',
+                  value: cartSubtotal,
+                },
+              },
+            },
+            items: cartItems.map((product) => {
+              return {
+                name: product.name,
+                unit_amount: {
+                  currency_code: 'USD',
+                  value: product.price,
+                },
+                quantity: product.quantity,
+              };
+            }),
+          },
+        ],
+      });
+    },
+    onCancel: onCancelHandler,
+    onApprove: function (data:any, actions:any) {
+      return actions.order.capture().then(function (orderData:any) {
+        var transaction = orderData.purchase_units[0].payments.captures[0];
+        if (
+          transaction.status === 'COMPLETED' &&
+          Number(transaction.amount.value) === Number(cartSubtotal)
+        ) {
+          updateOrder(orderId)
+            .then((data) => {
+              if (data.isPaid) {
+                updateStateAfterOrder(data.paidAt);
+              }
+            })
+            .catch((er) => console.log(er));
+        }
+      });
+    },
+    onError: onErrorHandler,
+  };
+};
+
+const onCancelHandler = function () {
+  console.log('cancel');
+};
+
+const onErrorHandler = function (err:any) {
+  console.log('error');
+};
+
+const updateOrder = async (orderId:number) => {
+  const { data } = await axios.put('/api/orders/paid/' + orderId);
+  return data;
+};
 
 const UserOrderDetailsPage = () => {
-    return <UserOrderDetailsPageComponent />;
-     
+  const userInfo = useSelector((state:any) => state.userRegisterLogin.userInfo);
+
+  const getUser = async () => {
+    const { data } = await axios.get('/api/users/profile/' + userInfo._id);
+    return data;
+  };
+
+  return (
+    <UserOrderDetailsPageComponent
+      userInfo={userInfo}
+      getUser={getUser}
+      getOrder={getOrder}
+      loadPayPalScript={loadPayPalScript}
+    />
+  );
 };
 
 export default UserOrderDetailsPage;
-
--- frontend/src/pages/user/components/UserOrderDetailsPageComponent.tsx
+++ frontend/src/pages/user/components/UserOrderDetailsPageComponent.tsx
@@ -8,8 +8,105 @@ import {
   Button,
 } from 'react-bootstrap';
 import CartItemComponent from '../../../components/CartItemComponent';
+import { useEffect, useRef, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
-const UserOrderDetailsPageComponent = () => {
+type UserOrderDetailsPageComponentProps = {
+  userInfo: any;
+  getUser(): Promise<any>;
+  getOrder(id: any): Promise<any>;
+  loadPayPalScript(a: any, b: any, c: any, updateStateAfterOrder : (paidAt: any) => void) : void
+  
+};
+const UserOrderDetailsPageComponent = ({
+  userInfo,
+  getUser,
+  getOrder,
+  loadPayPalScript,
+}: UserOrderDetailsPageComponentProps) => {
+  const [userAddress, setUserAddress] = useState<any>({});
+  const [paymentMethod, setPaymentMethod] = useState('');
+  const [isPaid, setIsPaid] = useState(false);
+  const [orderButtonMessage, setOrderButtonMessage] = useState('');
+  const [cartItems, setCartItems] = useState([]);
+  const [cartSubtotal, setCartSubtotal] = useState(0);
+  const [isDelivered, setIsDelivered] = useState(false);
+  const [buttonDisabled, setButtonDisabled] = useState(false);
+
+  const paypalContainer = useRef<HTMLDivElement | null>(null);
+
+  const { id } = useParams();
+
+  useEffect(() => {
+    getUser()
+      .then((data) => {
+        setUserAddress({
+          address: data.address,
+          city: data.city,
+          country: data.country,
+          zipCode: data.zipCode,
+          state: data.state,
+          phoneNumber: data.phoneNumber,
+        });
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  useEffect(() => {
+    getOrder(id)
+      .then((data) => {
+        setPaymentMethod(data.paymentMethod);
+        setCartItems(data.cartItems);
+        setCartSubtotal(data.orderTotal.cartSubtotal);
+        data.isDelivered
+          ? setIsDelivered(data.deliveredAt)
+          : setIsDelivered(false);
+        data.isPaid ? setIsPaid(data.paidAt) : setIsPaid(false);
+        if (data.isPaid) {
+          setOrderButtonMessage('Your order is finished');
+          setButtonDisabled(true);
+        } else {
+          if (data.paymentMethod === 'pp') {
+            setOrderButtonMessage('Pay for your order');
+          } else if (data.paymentMethod === 'cod') {
+            setButtonDisabled(true);
+            setOrderButtonMessage('Wait for your order. You pay on delivery');
+          }
+        }
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  const orderHandler = () => {
+    setButtonDisabled(true);
+    if (paymentMethod === 'pp') {
+      setOrderButtonMessage(
+        'To pay for your order click one of the buttons below'
+      );
+      if (!isPaid) {
+        loadPayPalScript(cartSubtotal, cartItems, id, updateStateAfterOrder);
+      }
+    } else {
+      setOrderButtonMessage('Your order was placed. Thank you');
+    }
+  };
+
+  const updateStateAfterOrder = (paidAt: boolean) => {
+    setOrderButtonMessage('Thank you for your payment!');
+    setIsPaid(paidAt);
+    setButtonDisabled(true);
+    if (paypalContainer.current) {
+      (paypalContainer.current as HTMLElement).style.display = 'none';
+    }
+  };
+  function removeFromCartHandler(a: any, b: any, c: any) {
+    throw new Error('Function not implemented.');
+  }
+
+  function changeCount(a: any, b: any): void {
+    throw new Error('Function not implemented.');
+  }
+
   return (
     <Container fluid>
       <Row className="mt-4">
@@ -19,13 +116,14 @@ const UserOrderDetailsPageComponent = () => {
           <Row>
             <Col md={6}>
               <h2>Shipping</h2>
-              <b>Name</b>: John Doe <br />
-              <b>Address</b>: 8739 Mayflower St. Los Angeles, CA 90063 <br />
-              <b>Phone</b>: 888 777 666
+              <b>Name</b>: {userInfo.name} {userInfo.lastName} <br />
+              <b>Address</b>: {userAddress.address} {userAddress.city}{' '}
+              {userAddress.state} {userAddress.zipCode} <br />
+              <b>Phone</b>: {userAddress.phoneNumber}
             </Col>
             <Col md={6}>
               <h2>Payment method</h2>
-              <Form.Select disabled={false}>
+              <Form.Select value={paymentMethod} disabled={true}>
                 <option value="pp">PayPal</option>
                 <option value="cod">
                   Cash On Delivery (delivery may be delayed)
@@ -34,13 +132,20 @@ const UserOrderDetailsPageComponent = () => {
             </Col>
             <Row>
               <Col>
-                <Alert className="mt-3" variant="danger">
-                  Not delivered
+                <Alert
+                  className="mt-3"
+                  variant={isDelivered ? 'success' : 'danger'}
+                >
+                  {isDelivered ? (
+                    <>Delivered at {isDelivered}</>
+                  ) : (
+                    <>Not delivered</>
+                  )}
                 </Alert>
               </Col>
               <Col>
-                <Alert className="mt-3" variant="success">
-                  Paid on 2022-10-02
+                <Alert className="mt-3" variant={isPaid ? 'success' : 'danger'}>
+                  {isPaid ? <>Paid on {isPaid}</> : <>Not paid yet</>}
                 </Alert>
               </Col>
             </Row>
@@ -48,20 +153,12 @@ const UserOrderDetailsPageComponent = () => {
           <br />
           <h2>Order items</h2>
           <ListGroup variant="flush">
-            {Array.from({ length: 3 }).map((item, idx) => (
+            {cartItems.map((item: any, idx: any) => (
               <CartItemComponent
-                item={{
-                  image: { path: '/images/tablets-category.png' },
-                  name: 'Product name',
-                  productID: idx,
-                  price: 10,
-                  count: 10,
-                  quantity: 10,
-                }}
+                item={item}
                 key={idx}
-                orderCreated={false}
-                removeFromCartHandler={() => {}}
-                changeCount={() => {}}
+                removeFromCartHandler={removeFromCartHandler}
+                changeCount={changeCount}
               />
             ))}
           </ListGroup>
@@ -72,7 +169,8 @@ const UserOrderDetailsPageComponent = () => {
               <h3>Order summary</h3>
             </ListGroup.Item>
             <ListGroup.Item>
-              Items price (after tax): <span className="fw-bold">$892</span>
+              Items price (after tax):{' '}
+              <span className="fw-bold">${cartSubtotal}</span>
             </ListGroup.Item>
             <ListGroup.Item>
               Shipping: <span className="fw-bold">included</span>
@@ -81,13 +179,22 @@ const UserOrderDetailsPageComponent = () => {
               Tax: <span className="fw-bold">included</span>
             </ListGroup.Item>
             <ListGroup.Item className="text-danger">
-              Total price: <span className="fw-bold">$904</span>
+              Total price: <span className="fw-bold">${cartSubtotal}</span>
             </ListGroup.Item>
             <ListGroup.Item>
               <div className="d-grid gap-2">
-                <Button size="lg" variant="danger" type="button">
-                  Pay for the order
+                <Button
+                  size="lg"
+                  onClick={orderHandler}
+                  variant="danger"
+                  type="button"
+                  disabled={buttonDisabled}
+                >
+                  {orderButtonMessage}
                 </Button>
+              </div>
+              <div style={{ position: 'relative', zIndex: 1 }}>
+                <div ref={paypalContainer} id="paypal-container-element"></div>
               </div>
             </ListGroup.Item>
           </ListGroup>
@@ -98,3 +205,5 @@ const UserOrderDetailsPageComponent = () => {
 };
 
 export default UserOrderDetailsPageComponent;
+
+
