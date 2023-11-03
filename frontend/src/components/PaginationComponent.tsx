35463cdcf247225aafb5ce2f4c1401fb691e10a7
Bechir-Marco <bechir.marko@gmail.com>
2023-11-04 00:23:41 +0100

    pagination, bestseller & categories in Header Comp

--- frontend/src/components/PaginationComponent.tsx
+++ frontend/src/components/PaginationComponent.tsx
@@ -1,4 +1,6 @@
 import { Pagination } from "react-bootstrap";
+import { LinkContainer } from 'react-router-bootstrap';
+
 type PaginationComponentProps = {
   categoryName: any;
   searchQuery: any;
@@ -12,19 +14,28 @@ const PaginationComponent = ({
   paginationLinksNumber,
   pageNum,
 }: PaginationComponentProps) => {
+   const category = categoryName ? `category/${categoryName}/` : '';
+   const search = searchQuery ? `search/${searchQuery}/` : '';
+   const url = `/product-list/${category}${search}`;
   return (
     <Pagination>
-      <Pagination.Prev />
-      <Pagination.Item>{1}</Pagination.Item>
-
-      <Pagination.Item>{10}</Pagination.Item>
-      <Pagination.Item>{11}</Pagination.Item>
-      <Pagination.Item active>{12}</Pagination.Item>
-      <Pagination.Item>{13}</Pagination.Item>
-      <Pagination.Item>{14}</Pagination.Item>
-
-      <Pagination.Item>{20}</Pagination.Item>
-      <Pagination.Next />
+      <LinkContainer to={`${url}${pageNum - 1}`}>
+        <Pagination.Prev disabled={pageNum === 1} />
+      </LinkContainer>
+      {[...Array(paginationLinksNumber).keys()].map((x) => (
+        <LinkContainer key={x + 1} to={`${url}${x + 1}`}>
+          <Pagination.Item active={x + 1 === pageNum}>{x + 1}</Pagination.Item>
+        </LinkContainer>
+      ))}
+      {pageNum === paginationLinksNumber ? (
+        <span>
+          <Pagination.Next disabled />
+        </span>
+      ) : (
+        <LinkContainer to={`${url}${pageNum + 1}`}>
+          <Pagination.Next />
+        </LinkContainer>
+      )}
     </Pagination>
   );
 };
